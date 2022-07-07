d256bd154cf5897e20cd37d5180c1afba8d79c18
Maximotus <dP=+Zl$2?P+7Z~#Ax-+3-$=1aOZ0^2([VUr$mA5VME[w0*cJQpQQ5kAC7BG&f,PUS.#pSSP21jP.q-g}Gs,6/F\{?\er{TnIF>
2022-07-07 15:13:15 +0200

    added first attempt of yaml config parsing

--- src/main.py
+++ src/main.py
@@ -1,2 +1,37 @@
+# TODO argument parsing
+"""
+arguments:
+    - learning rate
+    - batch_size
+    - epochs
+    - optimizer
+    - noise_size
+    - image_size
+    - use_spectral_norm
+    - dataset
+    - network / model
+    - model-specific parameters (dropout, alpha, beta, ...)
+    - device (gpu / cpu / auto)
+    - experiment_path
+        - experiment_path/stats
+        - experiment_path/ckpt
+        - experiment_path/logs
+    - frequencies
+        - save_freq (i.e. how often should the model checkpoint be saved)
+        - gen_freq (i.e. how often should test images be generated)
+"""
+
+import yaml
+
 if __name__ == '__main__':
-    pass
+    config = {}
+    with open("../config/test-config.yaml", "r") as stream:
+        try:
+            config = yaml.safe_load(stream)
+            print(config)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    print(config.get('learning_rate'))
+
+
