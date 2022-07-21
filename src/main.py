4f83b832f343efddecc9df0b4147cc705c586e87
Maximotus <dP=+Zl$2?P+7Z~#Ax-+3-$=1aOZ0^2([VUr$mA5VME[w0*cJQpQQ5kAC7BG&f,PUS.#pSSP21jP.q-g}Gs,6/F\{?\er{TnIF>
2022-07-21 11:47:42 +0200

    refactoring regarding directory structure

--- src/main.py
+++ src/main.py
@@ -41,14 +41,16 @@ def parse_config(argv):
     return config_path, config
 
 
-def create_experiment_dir(conf_file, exp_path):
+def create_experiment_dir(conf_file, exp_path, run_mode):
     config_name = conf_file[10:-5]
     now = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
-    base_path = os.path.join(exp_path, config_name, now)
+    base_path = os.path.join(exp_path, run_mode, config_name, now)
 
-    paths = [os.path.join(base_path, 'stats'),
-             os.path.join(base_path, 'model'),
-             os.path.join(base_path, 'results')]
+    paths = [os.path.join(base_path, 'results')]
+    if run_mode in MODES[0:2]:
+        paths.append(os.path.join(base_path, 'stats'))
+    if run_mode == MODES[0]:
+        paths.append(os.path.join(base_path, 'model'))
 
     for path in paths:
         os.makedirs(path)
@@ -63,9 +65,11 @@ def create_experiment_dir(conf_file, exp_path):
 if __name__ == '__main__':
     configuration_file, configuration = parse_config(sys.argv)
     mode = configuration.get(MODE_KEY)
-    experiment_path = create_experiment_dir(configuration_file, configuration.get(EXPERIMENT_PATH_KEY))
+    experiment_path = create_experiment_dir(configuration_file, configuration.get(EXPERIMENT_PATH_KEY), mode)
+
     logger = setup_logger(experiment_path)
     logger.info('Successfully read the given configuration file, created experiment directories and set up logger.')
+    logger.info('Starting experiment in mode ' + mode + ' using configuration ' + configuration_file)
 
     try:
         if mode not in MODES:
@@ -92,10 +96,5 @@ if __name__ == '__main__':
         if mode == MODES[2]:
             generator = PhotofitGenerator()
             generator.generate()
-    # except AttributeError as e:
-    #     exc_type, exc_obj, exc_tb = sys.exc_info()
-    #     file_name = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-    #     logger.error('Could not interpret configuration file properly or some other error occurred: ' + str(exc_type) + ' ' + str(file_name) +
-    #                  ' ' + str(exc_tb.tb_lineno) + ' ' + str(e))
     except (ConfigurationError, NotImplementedError) as e:
         logger.exception(e)
